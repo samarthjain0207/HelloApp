7a703ae69ba0c01ae97ccaed45237b6301ba1902
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-17 13:31:59 +0530

    UC2 comepleted

--- HelloApp.java
+++ HelloApp.java
@@ -2,6 +2,10 @@ public class HelloApp
 {
     public static void main (String [] args)
     {
-        System.out.println("Hello World!");
+        String name = args[0];
+        System.out.println("Hello "+name+"!");
+        /*Commands to be used 
+        javac HelloApp.java
+        java HelloApp Samarth */
     }
 }
