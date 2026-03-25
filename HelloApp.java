6541b1c4d85df5e73b10bf7b6b8343cd30650216
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-25 13:40:33 +0530

    UC7 completed

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,10 @@
 public class HelloApp {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            String finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-            System.out.println("Hello, " + finalNames + "!");
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
